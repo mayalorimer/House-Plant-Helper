b8d9dabc2d1c697b708c9fa58ebd039fab74d3ab
jennabuettner <jenna.e.buettner@gmail.com>
2022-07-08 15:52:01 -0700

    making schema

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+CREATE TABLE user (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(50) NOT NULL,
+    username VARCHAR (30) NOT NULL,
+    password VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE post (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    image BLOB NOT NULL,
+    description VARCHAR(3000) NOT NULL,
+    user_id INT,
+    FOREIGN KEY (user_id)
+    REFERENCES user(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE advice (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    advice VARCHAR(3000) NOT NULL,
+    user_id INT,
+    post_id INT,
+    FOREIGN KEY (user_id)
+    REFERENCES user(id),
+    FOREIGN KEY (post_id)
+    REFERENCES post(id)
+    ON DELETE SET NULL
+);
