9436430416ef20e3882824e98c6c5f71549e17d9
jennabuettner <jenna.e.buettner@gmail.com>
2022-07-09 16:58:29 -0700

    updated seeds and user

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO user(email, username, password)
+VALUES 
+('bob@bob.com', 'bob_is_cool', 'coolguy123'),
+('penny@penelope.com', 'plant_mama', 'monsterra22'),
+('john@doe.com', 'jonnyboy22', 'jd1989'),
+('jane@doe.com', 'houseplant.lover', 'h0useplants'),
+('frank@frankie.net', 'plants4ever', 'frankielovesplants123');
+
+INSERT INTO post(title, image, description, user)
+VALUES
+('')
