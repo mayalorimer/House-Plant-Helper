acf37ebd7f7f21c64240ee835aeae28962838fd5
Maya Lorimer <maya.lorimer@yahoo.com>
2022-07-12 10:33:36 -0700

    post and advice routes created

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { Post, Advice } = require('../../models');
 
+// post a new post
 router.post('/', async (req, res) => {
   try {
-    const newProject = await Project.create({
-      ...req.body,
-      user_id: req.session.user_id,
+    const newProject = await Post.create({
+      title: req.body.title,
+      description: req.body.description,
+      image: req.body.image,
+      user_id: req.session.user_id
     });
 
     res.status(200).json(newProject);
@@ -14,24 +17,20 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.delete('/:id', async (req, res) => {
+//create a comment / advice
+router.post('/advice', async (req, res) => {
   try {
-    const projectData = await Project.destroy({
-      where: {
-        id: req.params.id,
-        user_id: req.session.user_id,
-      },
+    const newAdvice = await Advice.create({
+      advice: req.body.advice, 
+      user_id: req.session.user_id,
+      post_id: req.body.post_id
     });
 
-    if (!projectData) {
-      res.status(404).json({ message: 'No project found with this id!' });
-      return;
-    }
-
-    res.status(200).json(projectData);
+    res.status(200).json(newAdvice);
   } catch (err) {
-    res.status(500).json(err);
+    res.status(400).json(err);
   }
 });
+
 
 module.exports = router;
